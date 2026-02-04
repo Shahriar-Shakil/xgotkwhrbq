c0725b5adb5a1106144bf798ce32fa36337dfab5
shahriar-shakil <toshahriarshakil@gmail.com>
2026-02-04 18:13:52 +0600

    add sidebar component from shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { AppSidebar } from "@/components/app-sidebar";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +25,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <SidebarProvider defaultOpen={false}>
+          <AppSidebar />
+          <SidebarInset className="w-full">
+            <main className="w-full min-h-screen">{children}</main>
+          </SidebarInset>
+        </SidebarProvider>
       </body>
     </html>
   );
